1968f6f5c832ed088fad78cdeb5db503c5d7d95a
Marko Rajević <markoarsenal@gmail.com>
2025-09-11 01:13:53 +0200

    -m 11.9.2025. -n

--- sketches/sketch-05.js
+++ sketches/sketch-05.js
@@ -1,4 +1,5 @@
 const canvasSketch = require("canvas-sketch");
+const random = require("canvas-sketch-util/random");
 
 const settings = {
   dimensions: [1080, 1080],
@@ -8,40 +9,104 @@ let manager;
 
 let text = "A";
 const fontFamily = "Serif";
-const fontSize = 1200;
+let fontSize = 1200;
 
-const sketch = () => {
+const typeCanvas = document.createElement("canvas");
+const typeContext = typeCanvas.getContext("2d");
+
+const sketch = ({ context, width, height }) => {
+  const cell = 20;
+  const cols = Math.floor(width / cell);
+  const rows = Math.floor(height / cell);
+  const cells = cols * rows;
+
+  typeCanvas.width = cols;
+  typeCanvas.height = rows;
+
   return ({ context, width, height }) => {
-    context.fillStyle = "white";
-    context.fillRect(0, 0, width, height);
+    typeContext.fillStyle = "black";
+    typeContext.fillRect(0, 0, cols, rows);
 
-    context.fillStyle = "black";
-    context.font = `${fontSize}px ${fontFamily}`;
-    context.textBaseline = "middle";
-    context.textAlign = "center";
+    fontSize = cols * 1.2;
 
-    const metrics = context.measureText(text);
+    typeContext.fillStyle = "white";
+    typeContext.font = `${fontSize}px ${fontFamily}`;
+    typeContext.textBaseline = "middle";
+    typeContext.textAlign = "center";
+
+    const metrics = typeContext.measureText(text);
     const mx = -metrics.actualBoundingBoxLeft;
     const my = -metrics.actualBoundingBoxAscent;
     const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
     const mh =
       metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
 
-    const x = (width - mw) / 2 - mx;
-    const y = (height - mh) / 2 - my;
+    const x = (cols - mw) / 2 - mx;
+    const y = (rows - mh) / 2 - my;
 
-    context.save();
+    typeContext.save();
 
-    context.translate(x, y);
+    typeContext.translate(x, y);
 
-    context.beginPath();
-    context.rect(mx, my, mw, mh);
-    context.stroke();
+    typeContext.beginPath();
+    typeContext.rect(mx, my, mw, mh);
+    typeContext.stroke();
 
-    context.fillText(text, 0, 0);
+    console.log("text", text, typeContext.fillStyle, metrics);
 
-    context.restore();
+    typeContext.fillText(text, 0, 0);
+
+    typeContext.restore();
+
+    const typeData = typeContext.getImageData(0, 0, cols, rows).data;
+
+    context.fillStyle = "black";
+    context.fillRect(0, 0, width, height);
+
+    context.textAlign = "center";
+    context.textBaseline = "middle";
+
+    for (let i = 0; i < cells; i++) {
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+
+      const x = col * cell;
+      const y = row * cell;
+
+      const red = typeData[i * 4];
+      const green = typeData[i * 4 + 1];
+      const blue = typeData[i * 4 + 2];
+
+      const glyph = getGlyph(red, text);
+
+      context.fillStyle = `rgb(${red}, ${green}, ${blue})`;
+
+      context.save();
+
+      context.translate(x, y);
+      context.translate(-cell * 0.5, -cell * 0.5);
+      // context.fillRect(0, 0, cell, cell);
+
+      context.font = `${cell * 2}px ${fontFamily}`;
+      if (Math.random() < 0.1) context.font = `${cell * 6}px ${fontFamily}`;
+
+      context.fillText(glyph, 0, 0);
+
+      context.restore();
+    }
   };
+};
+
+const getGlyph = (red, text) => {
+  if (red < 50) return "";
+  if (red < 100) return ".";
+  if (red < 150) return ",";
+  if (red < 200) return "!";
+  if (red < 250) return "?";
+
+  const glyphs = "_+/".split("");
+
+  return random.pick(glyphs);
 };
 
 document.addEventListener("keyup", (event) => {
